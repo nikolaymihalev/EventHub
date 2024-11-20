72e3fb7064624fa0a22fd23ced1a066865bcdb0e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-11-20 13:09:20 +0200

    Create RegistrationController.cs

---
+++ EventHub-API/EventHub.API/Controllers/RegistrationController.cs
@@ -0,0 +1,57 @@
+﻿using EventHub.API.Constants;
+using EventHub.Core.Contracts;
+using EventHub.Core.Models.EventRegistration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventHub.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly IEventRegistrationService eventRegistrationService;
+
+        public RegistrationController(IEventRegistrationService _eventRegistrationService)
+        {
+            eventRegistrationService = _eventRegistrationService;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetRegistrations(string userId)
+        {
+            var model = await eventRegistrationService.GetUserEventRegistrationsAsync(userId);
+
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]RegistrationFormModel model)
+        {
+            try
+            {
+                await eventRegistrationService.AddAsync(model);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok(new { Message = string.Format(SuccessfullMessages.Created, "Registration for event") });
+        }
+
+        [HttpDelete("{id}/user/{userId}")]
+        public async Task<IActionResult> Delete(int id, string userId)
+        {
+            try
+            {
+                await eventRegistrationService.DeleteAsync(id, userId);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok(new { Message = string.Format(SuccessfullMessages.Deleted, "Registration for event") });
+        }
+    }
+}
