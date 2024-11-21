b577bcbb00cc436f0f07559eb76feaeac4680c1f
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-11-21 11:51:47 +0200

    Added tests for Event service

---
+++ EventHub-API/EventHub.UnitTests/EventServiceTests.cs
@@ -0,0 +1,255 @@
+﻿using EventHub.Core.Constants;
+using EventHub.Core.Contracts;
+using EventHub.Core.Models.Comment;
+using EventHub.Core.Models.Event;
+using EventHub.Core.Services;
+using EventHub.Infrastructure.Common;
+using EventHub.Infrastructure.Data;
+using EventHub.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventHub.UnitTests
+{
+    [TestFixture]
+    public class EventServiceTests
+    {
+        private ApplicationDbContext context;
+        private IRepository repository;
+        private IEventService eventService;
+        private Event ev;
+        private static string userId = Guid.NewGuid().ToString();
+
+        [SetUp]
+        public void SetUp()
+        {
+            ev = new Event() { Id = 1, Title  = "Test" , Description = "Test Description", Date = new DateTime(2024, 12, 15), CreatorId = userId, CategoryId = 1, Location = "NY York"};
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+               .UseInMemoryDatabase(databaseName: "EventHubDb")
+               .Options;
+
+            this.context = new ApplicationDbContext(options);
+
+            this.repository = new Repository(this.context);
+
+            this.repository.AddAsync(ev);
+
+            this.repository.SaveChangesAsync();
+
+            eventService = new EventService(this.repository);
+        }
+
+        [Test]
+        public async Task Test_AddAsyncShouldAddModel()
+        {
+            int exEventsCount = 2;
+            string exTitle = "Add model";
+            string exDesc = "Add Description";
+            string exDate = "25.06.2024";
+            int exCategoryId = 3;
+            string exLocation = "Germany";
+
+            var eventModel = new EventFormModel()
+            {
+                Title = "Add model",
+                Description = "Add Description",
+                Date = new DateTime(2024, 06, 25),
+                CreatorId = userId,
+                CategoryId = 3,
+                Location = "Germany"
+            };
+
+            await eventService.AddAsync(eventModel);
+
+            var events = await eventService.GetEventsForPageAsync();
+
+            var addedEvent = events.Events.Last();
+
+            Assert.IsTrue(exEventsCount == events.Events.Count());
+            Assert.IsTrue(exTitle == addedEvent.Title);
+            Assert.IsTrue(exDesc == addedEvent.Description);
+            Assert.IsTrue(exDate == addedEvent.Date);
+            Assert.IsTrue(exCategoryId == addedEvent.CategoryId);
+            Assert.IsTrue(exLocation == addedEvent.Location);
+        }
+
+        [Test]
+        public void Test_AddAsyncShouldThrowException()
+        {
+            var exception = Assert.ThrowsAsync<ArgumentException>(async () => await eventService.AddAsync(null));
+
+            Assert.IsNotNull(exception.Message);
+            Assert.IsTrue(ErrorMessages.OperationFailedErrorMessage == exception.Message);
+        }
+
+        [Test]
+        public void Test_DeleteAsyncShouldThrowException_EventDoesntExist()
+        {
+            var exception = Assert.ThrowsAsync<ArgumentException>(async () => await eventService.DeleteAsync(55, userId));
+
+            Assert.IsNotNull(exception.Message);
+            Assert.IsTrue(string.Format(ErrorMessages.InvalidModelErrorMessage, "parameters") == exception.Message);
+        }
+
+        [Test]
+        public void Test_DeleteAsyncShouldThrowException_UserDoesntExist()
+        {
+            var exception = Assert.ThrowsAsync<ArgumentException>(async () => await eventService.DeleteAsync(1, ""));
+
+            Assert.IsNotNull(exception.Message);
+            Assert.IsTrue(string.Format(ErrorMessages.InvalidModelErrorMessage, "parameters") == exception.Message);
+        }
+
+        [Test]
+        public async Task Test_DeleteShoudDeleteComment()
+        {
+            int exEventCount = 0;
+
+            await eventService.DeleteAsync(ev.Id, userId);
+
+            var events = await eventService.GetEventsForPageAsync();
+
+            Assert.IsTrue(exEventCount == events.Events.Count());
+            Assert.IsTrue(exEventCount == events.PagesCount);
+        }
+
+        [Test]
+        public void Test_EditAsyncShouldThrowException_EventIsNull()
+        {
+            var exception = Assert.ThrowsAsync<ArgumentException>(async () => await eventService.EditAsync(new EventFormModel(), userId));
+
+            Assert.IsNotNull(exception.Message);
+            Assert.IsTrue(string.Format(ErrorMessages.InvalidModelErrorMessage, "parameters") == exception.Message);
+        }
+
+        [Test]
+        public void Test_EditAsyncShouldThrowException_UserDoesntExist()
+        {
+            var exception = Assert.ThrowsAsync<ArgumentException>(async () => await eventService.EditAsync(new EventFormModel() { Id = 1 }, ""));
+
+            Assert.IsNotNull(exception.Message);
+            Assert.IsTrue(string.Format(ErrorMessages.InvalidModelErrorMessage, "parameters") == exception.Message);
+        }
+
+        [Test]
+        public async Task Test_EditShoudEditComment()
+        {
+            string exTitle = "Edit model";
+            string exDesc = "Edit Description";
+            string exDate = "25.07.2024";
+            int exCategoryId = 3;
+            string exLocation = "France";
+
+            var newEv = new EventFormModel()
+            {
+                Id = 1,
+                Title = "Edit model",
+                Description = "Edit Description",
+                Date = new DateTime(2024, 07, 25),
+                CreatorId = userId,
+                CategoryId = 3,
+                Location = "France"
+            };
+
+            await eventService.EditAsync(newEv, userId);
+
+            var eventModel = await eventService.GetEventByIdAsync(1);
+
+            Assert.IsTrue(exTitle == eventModel.Title);
+            Assert.IsTrue(exDesc == eventModel.Description);
+            Assert.IsTrue(exDate == eventModel.Date);
+            Assert.IsTrue(exCategoryId == eventModel.CategoryId);
+            Assert.IsTrue(exLocation == eventModel.Location);
+        }
+
+        [Test]
+        public void Test_GetByIdShouldThrowException()
+        {
+            var exception = Assert.ThrowsAsync<ArgumentException>(async () => await eventService.GetEventByIdAsync(100));
+
+            Assert.IsNotNull(exception.Message);
+            Assert.IsTrue(string.Format(ErrorMessages.DoesntExistErrorMessage, "event") == exception.Message);
+        }
+
+        [Test]
+        public async Task Test_GetByIdShouldReturnTrueModel()
+        {
+            string exTitle = "Test";
+            string exDesc = "Test Description";
+            string exDate = "15.12.2024";
+            int exCategoryId = 1;
+            string exLocation = "NY York";
+
+            var eventModel = await eventService.GetEventByIdAsync(1);
+
+            Assert.IsTrue(exTitle == eventModel.Title);
+            Assert.IsTrue(exDesc == eventModel.Description);
+            Assert.IsTrue(exDate == eventModel.Date);
+            Assert.IsTrue(exCategoryId == eventModel.CategoryId);
+            Assert.IsTrue(exLocation == eventModel.Location);
+        }
+
+        [Test]
+        public async Task Test_GetEventsForPageShouldReturnTrueModel()
+        {
+            int exPagesCount = 1;
+            int exCurrentPage = 1;
+            int exEventsCount = 1;
+
+            var result = await eventService.GetEventsForPageAsync();
+
+            Assert.IsTrue(exCurrentPage == result.CurrentPage);
+            Assert.IsTrue(exPagesCount == result.PagesCount);
+            Assert.IsTrue(exEventsCount == result.Events.Count());
+        }
+
+        [Test]
+        public async Task Test_GetEventsForPageShouldReturnEmptyModel()
+        {
+            int exPagesCount = 1;
+            int exCurrentPage = 2;
+            int exEventsCount = 0;
+
+            var result = await eventService.GetEventsForPageAsync(2);
+
+            Assert.IsTrue(exCurrentPage == result.CurrentPage);
+            Assert.IsTrue(exPagesCount == result.PagesCount);
+            Assert.IsTrue(exEventsCount == result.Events.Count());
+        }
+
+        [Test]
+        public async Task Test_GetEventsForPageShouldReturnEmptyModelForUser()
+        {
+            int exPagesCount = 0;
+            int exCurrentPage = 1;
+            int exEventsCount = 0;
+
+            var result = await eventService.GetEventsForPageAsync(1, "");
+
+            Assert.IsTrue(exCurrentPage == result.CurrentPage);
+            Assert.IsTrue(exPagesCount == result.PagesCount);
+            Assert.IsTrue(exEventsCount == result.Events.Count());
+        }
+
+        [Test]
+        public async Task Test_GetEventsForPageShouldReturnTrueModelForUser()
+        {
+            int exPagesCount = 1;
+            int exCurrentPage = 1;
+            int exEventsCount = 1;
+
+            var result = await eventService.GetEventsForPageAsync(1, userId);
+
+            Assert.IsTrue(exCurrentPage == result.CurrentPage);
+            Assert.IsTrue(exPagesCount == result.PagesCount);
+            Assert.IsTrue(exEventsCount == result.Events.Count());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.context.Database.EnsureDeleted();
+            this.context.Dispose();
+        }
+    }
+}
