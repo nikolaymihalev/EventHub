150c715de7a85c55e0eca2e3749d64c5c2ab2b77
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-11-26 12:55:12 +0200

    Updated event list component

--- EventHub-FE/src/app/events/events-list/events-list.component.ts
+++ EventHub-FE/src/app/events/events-list/events-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../api.service';
 import { EventPageModel } from '../../types/eventsPageModel';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-events-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './events-list.component.html',
   styleUrl: './events-list.component.css'
 })
