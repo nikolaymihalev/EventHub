3cd534de1dfd874045828756940cab97ee3440e3
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-11-29 15:46:17 +0200

    Created error 404 component

--- EventHub-FE/src/app/app.routes.ts
+++ EventHub-FE/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: '404', component: ErrorComponent },
+    { path: '**', redirectTo: '/404' },
 ];
