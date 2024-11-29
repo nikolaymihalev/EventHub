60b35c61ae863c8457a2dc3df7917fa0686fe9f8
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-11-29 14:49:04 +0200

    Created register form component

--- EventHub-FE/src/app/app.routes.ts
+++ EventHub-FE/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
 ];
