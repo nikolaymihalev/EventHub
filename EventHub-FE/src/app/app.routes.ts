9556a829145c2aa75d683f3466615c16d6e7c4cd
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-12-03 18:26:54 +0200

    Created About component

--- EventHub-FE/src/app/app.routes.ts
+++ EventHub-FE/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { MainComponent } from './main/main.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { ErrorComponent } from './error/error.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainComponent },
+    { path: 'about', component: AboutComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: '404', component: ErrorComponent },
